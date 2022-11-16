f902708338857c9c1edccb5d71f820e2e44815c5
AnAnnoyinGoose <anannoyingoose@googlemail.com>
2022-11-16 22:14:29 +0100

    rewrote all
    Signed-off-by: AnAnnoyinGoose <anannoyingoose@googlemail.com>

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
-# bapl
+# BAPL (BAd Programming Language)
+
+## Side note
+BAPL is a programming language that is designed to be bad. It is a joke language, and is not meant to be taken seriously.
+It's a small project I made to just for fun, and will probably get abandoned at some point.
+
+## What is BAPL?
+BAPL is supposed to be a mix between C and typescript. It is a compiled language, and is compiled to C, and then compiled to machine code.
+It is a statically typed language, and has a lot of features that are not in C, but are in typescript.
+more in [syntax](#syntax)
+
+
+## Syntax 
+
+### Variables
+Variables are declared with the `var` keyword, their type is assinged with a `::` and their value is assigned with a `=`. 
+```bapl
+var::int x = 5
+var::string y = "Hello World"
+var::bool z = true
+var::float  a= 5.5
+var::char b = 'a'
+```
+### Referencing variables
+Variables can be referenced by using the `$` symbol, and then the variable name.
+```bapl
+var::int x = 5
+var::int y = $x
+```
+### Functions
+Functions are declared with the `func` keyword, their name is assigned with a `::` and their parameters are assigned with a `(` and `)`. 
+```bapl
+func main::void() {
+    cnsl << "Hello World";
+}
+```
+### If statements
+If statements are declared with the `if` keyword, their condition is assigned with a `(` and `)`, and their body is assigned with a `{` and `}`. 
+```bapl
+if (true) {
+    cnsl << "Hello World";
+}
+```
+### loops
+Loops are declared with the `loop` keyword, the type of loop is assigned with a `::` and their body is assigned with a `{` and `}`. 
+```bapl
+loop::while(true) {
+    cnsl << "Hello World";
+}
+``` 
+### Re declarations
+Re declarations are declared with the `!` keyword, their value is assigned with a `=`. 
+```bapl
+!x = 5
+```
+
+
---
+++ main.c
@@ -0,0 +1,265 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// This is a compiler for my custom language called Bad!!
+// The extension is .bapl
+// The language is compiled to C, then to assembly and then to machine code.
+// the compiler accepts the following arguments:
+// bapl statements are written in one line
+
+// -o <output file> - the output file
+// -c - compile only
+// -e - execute the program
+// -S - compile to assembly
+// -f - file name
+
+// syntax of bapl is as follows:
+// 2. every statement ends with a semicolon
+// 3. every variable is declared with the keyword "var"
+// 4. every variable is assigned a type with :: (double colon)
+// 5. every variable is assigned a value with =
+// 7. eg var::int x = 5; <- this is a variable declaration of int x = 5 in C
+// 8. print statements are written with the keyword "csn"
+// 9. eg csn << "Hello World"; <- this is a print statement in C
+// 10. if statements are written with the keyword "if"
+// 11. eg if::param(x == 5)::output(csn << "x is 5"); <- this is an if statement in C
+// 12. while statements are written with the keyword "while"
+// 13. eg while::param(x < 5)::output(csn << "x is less than 5"); <- this is a while statement in C
+// 14. for statements are written with the keyword "for"
+// 15. eg for::param(x = 0; x < 5; x++)::output(csn << "x is less than 5"); <- this is a for statement in C
+// 16. functions are written with the keyword "func"
+// 17. eg func::return_type(int)::name(main)::param(int argc, char** argv)::output(csn << "Hello World";) <- this is a function in C
+// 18. functions can be called with the keyword "call"
+// 19. eg call::name(main)::param(argc, argv); <- this is a function call in C
+
+// 20. the following are the types of variables:
+// 21. int - integer (32 bit)
+// 22. char - character (single character)
+// 23. double - double (64 bit)
+// 24. float - float (32 bit)
+// 25. bool - boolean (true or false)
+// 26. string - string (array of characters)
+// 27. void - void (nothing)
+
+// 28, Example program:
+// var::int x = 5;
+// var::int y = 10;
+// var::int z = x + y;
+// csn << "x + y = " << z;
+
+// Output: x + y = 15
+
+// 29. Example program2:
+// var::int x = 5;
+// var::int y = 10;
+// if::param(x>y)::output(csn << "x is greater than y");
+// else::output(csn << "x is less than y");
+
+// Output: x is less than y
+
+// 30. Example program3:
+// var::int x = 5;
+// var::int y = 10;
+// while::param(x < y)::output(csn << "x is less than y");
+// x++;
+
+// Output: x is less than y ( 5 times )
+
+
+
+char *output_file = NULL;
+char *input_file = NULL;
+bool compile_only = false;
+bool execute = false;
+bool compile_to_assembly = false;
+bool print_file = false;
+
+
+
+void printHelp() {
+    // asici art of "Bad!!"
+    printf("   ____              _  _  _ \n  |  _ \\            | || || | \n  | |_) |  __ _   __| || || | \n  |  _ <  / _` | / _` || || | \n  | |_) || (_| || (_| ||_||_| \n  |____/  \\__,_| \\__,_|(_)(_)\n");
+    printf("Bad!! Compiler v0.1 by @AnAnnoyinGoose\n");
+    printf("Usage: bad-compile [options] [file]\n");
+    printf("Options:\n");
+    printf("  -o <output file> the output file\n");
+    printf("  -c compile only\n");
+    printf("  -e execute the program\n");
+    printf("  -S compile to assembly\n");
+    printf("  -f <filename.bapl || filename> input file\n");
+    printf("  -i print the file content\n");
+}
+
+
+// compiler
+
+int compiler (char *input_file, char *output_file, bool compile_only, bool execute, bool compile_to_assembly, bool print_file) {
+//    rewrite the bapl code to C
+printf("Compiling to C...\n");
+
+    FILE *input = fopen(input_file, "r");
+    FILE *output = fopen(output_file, "w");
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
+
+    if (input == NULL) {
+        printf("Error: I File not found\n");
+        return 1;
+    }
+    if (output == NULL) {
+        printf("Error: O File not found\n");
+        return 1;
+    }
+    fprintf(output,"#include <stdio.h>\n#include <string.h>\n#include <stdbool.h>\n#include <stdlib.h>\nint main(int argc, char** argv) {\n");
+    while ((read = getline(&line, &len, input)) != -1) {
+
+        printf("Retrieved line of length %zu: %s \n", read, line);
+        // csn << "Hello World";
+        if (strstr(line, "csn <<") != NULL) {
+            char *str = strstr(line, "csn <<");
+            str = str + 6;
+            // remove all the spaces around the string
+            while (str[0] == ' ') {
+                str = str + 1;
+            }
+            while (str[strlen(str) - 1] == ' ') {
+                str[strlen(str) - 1] = '\0';
+            }
+
+            fprintf(output, "printf(%s);\n", str);
+        }
+        // var::int x = 5;
+        if (strstr(line, "var::") != NULL) {
+            char *str = strstr(line, "var::");
+            str = str + 5;
+            // remove all the spaces around the string
+            while (str[0] == ' ') {
+                str = str + 1;
+            }
+            while (str[strlen(str) - 1] == ' ') {
+                str[strlen(str) - 1] = '\0';
+            }
+            // get the type
+            char *type = strtok(str, " ");
+            // get the name
+            char *name = strtok(NULL, " ");
+            // get the value
+            char *value = strtok(NULL, " ");
+            // remove all the spaces around the string
+            while (value[0] == ' ') {
+                value = value + 1;
+            }
+            while (value[strlen(value) - 1] == ' ') {
+                value[strlen(value) - 1] = '\0';
+            }
+            // remove the semicolon
+            value[strlen(value) - 1] = '\0';
+            fprintf(output, "%s %s = %s;\n", type, name, value);
+        }
+        // if::param(x>y)::output(csn << "x is greater than y");
+        if (strstr(line, "if::") != NULL) {
+            char *str = strstr(line, "if::");
+            str = str + 4;
+            // remove all the spaces around the string
+            while (str[0] == ' ') {
+                str = str + 1;
+            }
+            while (str[strlen(str) - 1] == ' ') {
+                str[strlen(str) - 1] = '\0';
+            }
+            // get the param
+            char *param = strtok(str, "::");
+            // get the output
+            char *output = strtok(NULL, "::");
+            // remove all the spaces around the string
+            while (param[0] == ' ') {
+                param = param + 1;
+            }
+            while (param[strlen(param) - 1] == ' ') {
+                param[strlen(param) - 1] = '\0';
+            }
+            while (output[0] == ' ') {
+                output = output + 1;
+            }
+            while (output[strlen(output) - 1] == ' ') {
+                output[strlen(output) - 1] = '\0';
+            }
+            // remove the semicolon
+            output[strlen(output) - 1] = '\0';
+            fprintf(output, "if(%s){\n%s\n}\n", param, output);
+
+
+    }
+    fprintf(output, "return 0;\n}");
+    fclose(input);
+    fclose(output);
+    if (line) {
+        free(line);
+    }
+    printf("Done!\n");
+    return 0;
+}
+
+
+
+int main(int argc, char *argv[]) {
+    // check if there are any arguments
+    if (argc == 1) {
+        printHelp();
+        return 0;
+    } else{
+        for (int i = 0; i < argc; i++) {
+            if (strcmp(argv[i], "-o") == 0) {
+                printf("output file: %s \n", argv[i+1]);
+                output_file = argv[i+1];
+            } else if (strcmp(argv[i], "-c") == 0) {
+                printf("compile only \n");
+                compile_only = true;
+            } else if (strcmp(argv[i], "-e") == 0) {
+                printf("execute \n");
+                execute = true;
+            } else if (strcmp(argv[i], "-S") == 0) {
+                printf("compile to assembly \n");
+                compile_to_assembly = true;
+            } else if (strcmp(argv[i], "-f") == 0) {
+                // check if the file is a .bapl file
+                if (strstr(argv[i+1], ".bapl") != NULL) {
+                    printf("input file: %s \n", argv[i+1]);
+                    input_file = argv[i+1];
+                } else {
+                    printf("input file: %s.bapl \n", argv[i+1]);
+                    input_file = strcat(argv[i+1], ".bapl");
+                }
+            } else if (strcmp(argv[i], "-h") == 0) {
+                printHelp();
+            } else if (strcmp(argv[i], "-i") == 0) {
+                print_file = true;
+            }
+        }
+    }
+
+
+// compile to C
+    compiler(input_file, output_file, compile_only, execute, compile_to_assembly, print_file);
+    // compile the C code
+    if (compile_only) {
+        system("gcc -o output " "test.c");
+    }
+    // compile to assembly
+    if (compile_to_assembly) {
+        system("gcc -S -o output.s " "test.c");
+    }
+    // run the C code
+    if (execute) {
+        system("clear");
+        system("./output");
+    }
+    // print the C code
+    if (print_file) {
+        system("cat output.c");
+    }
+    return 0;
+}
